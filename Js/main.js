f55e6d146a60fd37d06059777e1eb148705e19d2
Joel Ekeng <joelekeng23@gmail.com>
2023-10-14 11:34:41 +0000

    Added javascript for perloader

---
+++ Js/main.js
@@ -0,0 +1,16 @@
+// Set the duration in milliseconds (e.g., 5000 ms for 5 seconds)
+const preloaderDuration = 90000;
+const preloader = document.querySelector(".preloader");
+const main = document.querySelector(".main");
+
+// Hide the preloader after the specified duration, regardless of content loading
+setTimeout(function () {
+    preloader.style.display = "none";
+    main.style.display = "block";
+}, preloaderDuration);
+
+// Additional code to hide the preloader when the content is fully loaded
+window.addEventListener("load", function () {
+    preloader.style.display = "none";
+    main.style.display = "block";
+});
